7f7ecb0ef5655af9e021bba764fe958ebeabdced
Robert <22194161+robert-leitl@users.noreply.github.com>
2023-02-25 12:03:35 +0100

    define octahedron sample macro

--- src/app/shader/util/wrap-octahedron.glsl
+++ src/app/shader/util/wrap-octahedron.glsl
@@ -1,3 +1,9 @@
+/**
+ * This function correctly wraps the coordinates of
+ * an octahedral mapped texture, so that seams are
+ * minimized when a filter exceeds the edges of the
+ * texture.
+ */
 vec2 wrapOctahedron(vec2 uv) {
   vec2 st = abs(fract(uv * 0.5 - 0.5) * 2. - 1.);
   vec2 flip = sign(1. - abs(uv.xy * 2. - 1.));
